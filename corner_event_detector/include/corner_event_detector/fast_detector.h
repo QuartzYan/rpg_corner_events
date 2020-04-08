0ab7de37a38d363ad43f1ffd14c51772f7f9efdd
Quartz <thesuperstring@outlook.com>
2020-04-08 16:59:21 +0800

    Update fast_detector.h

--- corner_event_detector/include/corner_event_detector/fast_detector.h
+++ corner_event_detector/include/corner_event_detector/fast_detector.h
@@ -29,8 +29,8 @@ private:
   int circle4_[20][2];
 
   // parameters
-  static const int sensor_width_ = 240;
-  static const int sensor_height_ = 180;
+  static const int sensor_width_ = 346;
+  static const int sensor_height_ = 260;
 };
 
 
